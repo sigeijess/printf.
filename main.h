168f3fbc647e7e926452f11f015548fa32a17175
dominic6809 <124082859+dominic6809@users.noreply.github.com>
2023-03-31 20:00:35 +0300

    Update main.h

--- main.h
+++ main.h
@@ -24,12 +24,12 @@
  * @fmt: The format.
  * @fn: The function associated.
  */
+
 struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
-
 
 /**
  * typedef struct fmt fmt_t - Struct op
@@ -37,6 +37,7 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
+
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
